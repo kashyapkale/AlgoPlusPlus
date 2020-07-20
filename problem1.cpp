954651a757b5150cbdc96e14bdc49ea0ff47c0a0
kashyapkale <meetkashyap@outlook.com>
2020-07-20 13:39:38 +0530

    https://online.codingblocks.com/app/player/113373/content/109003/5268/code-challenge

---
+++ problem1.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<algorithm>
+#define lli long long int
+using namespace std;
+
+lli scholarship(lli N,lli M,lli X,lli Y){
+    lli s = 0;
+    lli e = N;
+    lli mid = e/2;
+    lli ans = -1;
+
+    while(s<=e){
+		mid = (e + s)/2;
+        if(mid*X <= M + ((N-mid)*Y)){
+            ans = mid;
+            s = mid + 1;
+        }
+        else{
+            e = mid - 1;
+        }
+    }
+
+    return ans;
+
+}
+
+
+
+int main(){
+    lli N,M,X,Y;
+    cin>>N>>M>>X>>Y;
+    cout<<scholarship(N,M,X,Y);
+}
