8c1a89b74f3e0e3509ae148b139e6e1a3aef87b2
kashyapkale <meetkashyap@outlook.com>
2020-07-27 20:55:54 +0530

    LinkedList Operations

---
+++ linkedlist.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<algorithm>
+#define lli long long int
+using namespace std;
+
+//LinkedList Class
+class node{
+    public:
+        int data;
+        node*next;
+        node(int d){
+            data = d;
+            next = NULL;
+        }
+};
+
+//Function to insert data at the tail
+void insertdata(int data,node* &head){
+    node* curr=head;
+    if(curr == NULL){
+        head = new node(data);
+    }
+    else{
+        while(curr->next != NULL){
+            curr = curr->next;
+        }
+        curr->next = new node(data);
+    }
+    //return head;
+}
+
+//function to print linked list
+void displayLinkedList(node* head){
+    node* curr=head;
+    if(curr == NULL){
+        cout<<"Empty Linked List"<<endl;
+    }
+    else{
+        while(curr->next != NULL){
+            cout<<curr->data<<"->";          
+            curr = curr->next;
+        }
+        cout<<curr->data<<endl;
+    }
+}
+
+
+void takeInput(node* &head){
+    int d;
+    cin>>d;
+    while(d!=-1){
+        insertdata(d,head);
+        cin>>d;
+    }
+}
+
+ostream& operator<<(ostream &os,node* head){
+    displayLinkedList(head);
+    return os;
+}
+
+istream& operator>>(istream &is,node* &head){
+    takeInput(head);
+    return is;
+}
+
+void reverseList(node* head){
+    node* curr=head;
+    node* temp;
+    node* prev;
+    if(curr == NULL){
+        cout<<"Empty Linked List"<<endl;
+    }
+    else{
+        while(curr->next != NULL){
+            
+        }
+        cout<<curr->data<<endl;
+    }
+}
+
+
+
+int main(){
+    
+    node* head = NULL;
+    
+    cin>>head;
+    
+    cout<<head;
+
+}
